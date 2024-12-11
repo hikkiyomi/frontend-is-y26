031a7fe9fc24825b7ea889a5d3a0056841b42df3
Mikhail Mefodyev <367394@niuitmo.ru>
2024-12-12 01:04:46 +0300

    [lab-4] feat: add underlining current location

--- js/pageLoad.js
+++ js/pageLoad.js
@@ -1,13 +1,33 @@
+const measureLoadTime = (startTime) => {
+  const endTime = performance.now();
+  const loadTime = ((endTime - startTime) / 1000).toFixed(4);
+
+  const element = document.getElementById("page-load-time");
+  const text = document.createTextNode(`Page load time: ${loadTime} seconds`);
+
+  element.appendChild(text);
+};
+
+const decorateNavCurrentPage = () => {
+  const location = document.location.href;
+  const elements = document.querySelectorAll(".nav__item");
+
+  elements.forEach((element) => {
+    console.log(element.href);
+    console.log(location);
+
+    if (element.href === location) {
+      element.style.textDecoration = "underline";
+      element.style.textDecorationThickness = "3px";
+    }
+  });
+};
+
 (function () {
   const startTime = performance.now();
 
   window.onload = () => {
-    const endTime = performance.now();
-    const loadTime = (endTime - startTime) / 1000;
-
-    const element = document.getElementById("page-load-time");
-    const text = document.createTextNode(`Page load time: ${loadTime}`);
-
-    element.appendChild(text);
+    measureLoadTime(startTime);
+    decorateNavCurrentPage();
   };
 })();
